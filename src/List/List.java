c8feee363a34b95d4be66cd4fc2fab8aa80eb6ff
Unchaptered <workstation19961002@gmail.com>
2022-01-04 09:20:57 +0900

    List Interface

--- src/List/List.java
+++ src/List/List.java
@@ -12,18 +12,28 @@ package List;
  */
 public interface List<E> {
 	
-	/**
-	 * List 에 E 원소 추가
-	 */
 	boolean add(E value);
-	
-	/**
-	 * List 의 특정 index 에 원소추가
-	 */
 	void add(int index, E value);
+	
 	E remove(int index);
 	boolean remove(Object value);
+	
 	E get(int index);
 	void set(int index, E value);
 	
+	boolean contains(Object value);
+	
+	/**
+	 * List 의 총 원소 수 반환
+	 */
+	int indexOf(Object value);
+
+	
+	int size();
+	
+	boolean isEmpty();
+	
+	public void clear();
+	
+	E remove();
 }
